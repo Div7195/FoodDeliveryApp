daf7b837877fd5e1441a91b4bb39ca10a983a327
N1ckool <93426859+N1ckool@users.noreply.github.com>
2023-06-11 00:36:11 +0530

    Eighth Commit

---
+++ app/src/main/java/div/appd/divfoodzdeliveryapp/OrderViewUser.java
@@ -0,0 +1,16 @@
+package div.appd.divfoodzdeliveryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fooddelivery.R;
+
+public class OrderViewUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_view_user);
+    }
+}
